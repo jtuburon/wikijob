7b960dae09e78c63a9671e821e66317cd2c7fcdd
Julio Mario <jtuburon@gmail.com>
2015-10-02 20:02:46 -0500

    Regexp patterns replaced to Infobox lines splitting

--- src/main/java/uniandes/bigdata/taller02/FilteringPatterns.java
+++ src/main/java/uniandes/bigdata/taller02/FilteringPatterns.java
@@ -20,7 +20,25 @@ import java.util.regex.Pattern;
 public class FilteringPatterns {    
     public static Pattern INFO_BOX_PATTERN = Pattern.compile("\\{\\{Infobox\\s+(.*?)'''", Pattern.DOTALL);
     
-    public static Pattern BIRTH_PLACE_PATTERN = Pattern.compile("\\|(\\s*)birth_place(\\s*)=(\\s*)(((,\\s+)?\\[\\[(.*)\\]\\])|(,\\s+)?(\\w+))+(\\s+)?\n");
+    public static String BIRTH_PLACE_TOKEN="birth_place";
+    public static String DEATH_PLACE_TOKEN="death_place";
+    public static String BIRTH_DATE_TOKEN="birth_date";
+    public static String DEATH_DATE_TOKEN="death_date";
+    public static String SPOUSE_TOKEN="spouse";
+    public static String CHILDREN_TOKEN="children";
+    
+    
+    public static String BIRTH_PLACE_KEY="born_in";
+    public static String DEATH_PLACE_KEY="died_in";
+    public static String BIRTH_DATE_KEY="born_on";
+    public static String DEATH_DATE_KEY="died_on";
+    public static String SPOUSE_KEY="married_with";
+    public static String CHILDREN_KEY="son_or_daughter";
+    
+    
+    
+    
+    public static Pattern BIRTH_PLACE_PATTERN = Pattern.compile("\\|(\\s*?)birth_place(\\s*?)=(\\s*?)(((,\\s+)?\\[\\[(.*?)\\]\\])|(,\\s+)?(\\w+))+(\\s+)?\n");
     public static Pattern DEATH_PLACE_PATTERN = Pattern.compile("\\|(\\s*)death_place(\\s*)=(\\s*)(((,\\s+)?\\[\\[(.*)\\]\\])|(,\\s+)?(\\w+))+(\\s+)?\n");
     
     public static Pattern BIRTH_DATE_PATTERN = Pattern.compile("\\|(\\s*)birth_date(\\s*)=(\\s*)\\{\\{(.*?)\\}\\}\n");
--- src/main/java/uniandes/bigdata/taller02/WikiMapper.java
+++ src/main/java/uniandes/bigdata/taller02/WikiMapper.java
@@ -8,6 +8,7 @@ package uniandes.bigdata.taller02;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.logging.Level;
@@ -54,16 +55,131 @@ public class WikiMapper extends Mapper<LongWritable, Text, Text, Text> {
             boolean isAPerson = m.find();
             boolean children_added = false;
             boolean spouses_added = false;
-
-            if (isAPerson) {
-
+            ArrayList<String> spouses = new ArrayList<>();;
+            ArrayList<String> children = new ArrayList<>();;
+            
+            if (isAPerson) {                
+                String[] lines = infoBox.split("\n");
+                HashMap<String, String> map= new HashMap<>();
+                for (int i = 0; i < lines.length; i++) {
+                    String line = lines[i];
+                    System.out.println("Line: "+ line);
+                    String[] line_vec = line.split("=", 2);
+                    if (line_vec.length == 2) {
+                        String l_key = line_vec[0];
+                        String l_val = line_vec[1];
+                        if (l_key.contains(FilteringPatterns.BIRTH_PLACE_TOKEN)) {
+                            if (l_val.contains(country)) {
+                                map.put(FilteringPatterns.BIRTH_PLACE_KEY, country);
+                            }
+                        }
+                        if (l_key.contains(FilteringPatterns.DEATH_PLACE_TOKEN)) {
+                            if (l_val.contains(country)) {
+                                map.put(FilteringPatterns.DEATH_PLACE_KEY, country);
+                            }
+                        }
+                        
+                        if(l_key.contains(FilteringPatterns.BIRTH_DATE_TOKEN)){
+                            String birthDateString = extractDateAsString(l_val);
+                            if (birthDateString != null) {
+                                Date birthDate = convertToDate(birthDateString);
+                                if (birthDate != null) {
+                                    if (birthDate.after(startDate) && birthDate.before(endDate)) {
+                                        map.put(FilteringPatterns.BIRTH_DATE_KEY, birthDateString);
+                                    }
+                                }
+                            }
+                        }
+                        
+                        if(l_key.contains(FilteringPatterns.DEATH_DATE_TOKEN)){
+                            String deathDateString = extractDateAsString(l_val);
+                            if (deathDateString != null) {
+                                Date deathDate = convertToDate(deathDateString);
+                                if (deathDate != null) {
+                                    if (deathDate.after(startDate) && deathDate.before(endDate)) {
+                                        map.put(FilteringPatterns.DEATH_DATE_KEY, deathDateString);
+                                    }
+                                }
+                            }
+                        }
+                        
+                        if(l_key.contains(FilteringPatterns.SPOUSE_TOKEN)){
+                            map.put(FilteringPatterns.SPOUSE_KEY, "");
+                            
+                            Matcher personMatcher = FilteringPatterns.SPOUSE_PATTERN_PERSON.matcher(l_val);
+                            while (personMatcher.find()) {
+                                String s_person = personMatcher.group("personinfo");
+                                s_person = s_person.split("\\|")[0];
+                                spouses.add(s_person);
+                            }
+                        }
+                        
+                        if(l_key.contains(FilteringPatterns.CHILDREN_TOKEN)){
+                            map.put(FilteringPatterns.CHILDREN_KEY, "");
+                            
+                            Matcher personMatcher = FilteringPatterns.CHILDREN_PATTERN_PERSON.matcher(l_val);
+                            while (personMatcher.find()) {
+                                String s_person = personMatcher.group("personinfo");
+                                s_person = s_person.split("\\|")[0];
+                                children.add(s_person);
+                            }
+                        }
+                    }
+                }
+                String person = extractPersonName(article);
+                if(map.containsKey(FilteringPatterns.BIRTH_PLACE_KEY)){
+                    context.write(new Text(person), new Text(FilteringPatterns.BIRTH_PLACE_KEY+ ":" + country));
+                    if(map.containsKey(FilteringPatterns.BIRTH_DATE_KEY)){
+                        context.write(new Text(person), new Text(FilteringPatterns.BIRTH_DATE_KEY+ ":"  + map.get(FilteringPatterns.BIRTH_DATE_KEY)));
+                    }
+                    if(map.containsKey(FilteringPatterns.SPOUSE_KEY) && spouses.size()>0){
+                        for (int i = 0; i < spouses.size(); i++) {
+                            String spouse = spouses.get(i);
+                            context.write(new Text(person), new Text(FilteringPatterns.SPOUSE_KEY + ":" + spouse));
+                        }
+                        spouses_added=true;
+                    }
+                    
+                    if(map.containsKey(FilteringPatterns.CHILDREN_KEY) && children.size()>0){
+                        for (int i = 0; i < children.size(); i++) {
+                            String child = children.get(i);
+                            context.write(new Text(person), new Text(FilteringPatterns.CHILDREN_KEY + ":" + child));
+                        }
+                        children_added=true;
+                    }
+                }
+                
+                if(map.containsKey(FilteringPatterns.DEATH_PLACE_KEY)){                    
+                    context.write(new Text(person), new Text(FilteringPatterns.DEATH_PLACE_KEY+ ":" + country));
+                    if(map.containsKey(FilteringPatterns.DEATH_DATE_KEY)){
+                        context.write(new Text(person), new Text(FilteringPatterns.DEATH_DATE_KEY+ ":" + map.get(FilteringPatterns.DEATH_DATE_KEY)));
+                    }
+                    if (!spouses_added) {
+                        if (map.containsKey(FilteringPatterns.SPOUSE_KEY) && spouses.size() > 0) {
+                            for (int i = 0; i < spouses.size(); i++) {
+                                String spouse = spouses.get(i);
+                                context.write(new Text(person), new Text(FilteringPatterns.SPOUSE_KEY + ":" + spouse));
+                            }
+                        }
+                    }
+                    
+                    if (!children_added) {
+                        if (map.containsKey(FilteringPatterns.CHILDREN_KEY) && children.size() > 0) {
+                            for (int i = 0; i < children.size(); i++) {
+                                String child = children.get(i);
+                                context.write(new Text(person), new Text(FilteringPatterns.CHILDREN_KEY + ":" + child));
+                            }
+                        }
+                    }
+                }
+                /*
                 Matcher birthPlaceMatcher = FilteringPatterns.BIRTH_PLACE_PATTERN.matcher(infoBox);
                 if (birthPlaceMatcher.find()) {
                     String birthPlaceValue = birthPlaceMatcher.group();
                     if (birthPlaceValue.contains(country)) {
                         String person = extractPersonName(article);
                         context.write(new Text(person), new Text("born_in: " + country));
-
+                        
                         Matcher birthDateMatcher = FilteringPatterns.BIRTH_DATE_PATTERN.matcher(infoBox);
                         if (birthDateMatcher.find()) {
                             String birthDateValue = birthDateMatcher.group(birthDateMatcher.groupCount());
@@ -101,10 +217,9 @@ public class WikiMapper extends Mapper<LongWritable, Text, Text, Text> {
                                 children_added = true;
                             }
                         }
-
                     }
                 }
-
+                
                 Matcher deathPlaceMatcher = FilteringPatterns.DEATH_PLACE_PATTERN.matcher(infoBox);
                 if (deathPlaceMatcher.find()) {
                     String deathPlaceValue = deathPlaceMatcher.group();
@@ -155,9 +270,9 @@ public class WikiMapper extends Mapper<LongWritable, Text, Text, Text> {
 
                     }
                 }
-
+                */
+            
             }
-
         }
 
     }
@@ -192,6 +307,7 @@ public class WikiMapper extends Mapper<LongWritable, Text, Text, Text> {
             date = DATE_FORMAT.parse(dateString);
             return date;
         } catch (ParseException ex) {
+            ex.printStackTrace();
         }
         return null;
     }
